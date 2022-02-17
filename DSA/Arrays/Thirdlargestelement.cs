974584743423dd2f1b7016669a80ec50f1fa4706
Maruthi Maramreddy <maruthi835@gmail.com>
2022-02-17 17:26:29 +0530

    Arrays / Third largest element

---
+++ DSA/Arrays/Thirdlargestelement.cs
@@ -0,0 +1,44 @@
+﻿namespace DSA.Arrays
+{
+    /*
+        Problem Statement:
+            Given an array of distinct elements. Find the third largest element in it.
+            Suppose you have A[] = {1, 2, 3, 4, 5, 6, 7}, its output will be 5 because 
+            it is the 3 largest element in the array A.
+        Example 1:
+            Input:
+                N = 5
+                A[] = {2,4,1,3,5}
+            Output: 3          
+        Example 2:
+            Input:
+                N = 2
+                A[] = {10,2}
+            Output: -1
+    */
+    public class Thirdlargestelement
+    {
+        public int ThirdLargest(int[] a, int n)
+        {
+            if (n < 3)
+            {
+                return -1;
+            }
+            // Sorted first 3 elements of the array in descending order. Complexity is O(n2)
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (a[i] < a[j])
+                    {
+                        int temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+
+            return a[2];
+        }
+    }
+}
