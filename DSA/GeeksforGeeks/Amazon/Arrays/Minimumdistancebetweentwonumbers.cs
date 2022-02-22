1e15e4bda17eec517eb1097228bfe2d2496bd75e
Maruthi Maramreddy <maruthi835@gmail.com>
2022-02-22 18:58:10 +0530

    Minimum distance between two numbers

---
+++ DSA/GeeksforGeeks/Amazon/Arrays/Minimumdistancebetweentwonumbers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DSA.GeeksforGeeks.Amazon.Arrays
+{
+    /*
+      Problem Statement:
+     You are given an array A, of N elements. Find minimum index based distance between two elements of the array, x and y.
+      Example 1:
+          Input:
+            N = 4
+            A[] = {1,2,3,2}
+            x = 1, y = 2
+            Output: 1
+            Explanation: x = 1 and y = 2. There are
+            two distances between x and y, which are
+            1 and 3 out of which the least is 1.
+      Example 2:
+          Input:
+            N = 7
+            A[] = {86,39,90,67,84,66,62}
+            x = 42, y = 12
+            Output: -1
+            Explanation: x = 42 and y = 12. We return
+            -1 as x and y don't exist in the array.
+   */
+    public class Minimumdistancebetweentwonumbers
+    {
+        public int minDist(int[] a, int n, int x, int y)
+        {
+            // code here
+
+            int firstIndex = -1;
+            int secondIndex = -1;
+            int minLength = -1;
+
+            for (int i=0; i<n; i++)
+            {
+                if(a[i] == x)
+                {
+                    firstIndex = i;
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (a[j] == y)
+                        {
+                            secondIndex = j;
+                            var length = Math.Abs(j - i);
+                            if(minLength > length || minLength == -1)
+                                minLength = length;
+
+                        }
+                    }
+                }                
+            }
+
+            return minLength;
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,4 +1,5 @@
-﻿using DSA.Leetcode.Algorithm_I.Day1;
+﻿using DSA.GeeksforGeeks.Amazon.Arrays;
+using DSA.Leetcode.Algorithm_I.Day1;
 using System;
 
 namespace DSA
@@ -7,9 +8,9 @@ namespace DSA
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 3,5,6};
-            SearchInsertPosition35 searchInsertPosition35 = new SearchInsertPosition35();
-            int result = searchInsertPosition35.SearchInsert(array, 7);
+            int[] array = { 98, 78, 10, 12, 59, 37, 45, 18, 1, 56, 37, 14, 3, 32, 85, 10, 69, 89, 29, 93, 44, 16, 26, 13, 50, 75, 79, 21, 20, 33, 55, 17, 63, 64, 80, 21, 52, 24, 90, 52, 80, 26, 18, 34, 57, 2, 95, 25, 42, 23, 17, 85, 39, 94, 50, 40, 21, 28, 12, 40, 61, 67, 9, 23, 30, 88, 95, 34, 64, 85, 85, 95, 62, 54, 28, 19, 55, 22, 95, 49, 97, 64, 33 };
+            Minimumdistancebetweentwonumbers minimumdistancebetweentwonumbers = new Minimumdistancebetweentwonumbers();
+            var result = minimumdistancebetweentwonumbers.minDist(array, 83, 34, 56);
 
             Console.WriteLine(result);
             Console.WriteLine("Hello World!");
